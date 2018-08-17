8e856eb52234f281b42f6583babe11b49f38bd32
Bykadorov, Roman <prosto_mail95@mail.ru>
2018-08-17 18:50:28 +0300

    [Parser] Initial dumping/demuxing fixes

--- src/Source.cpp
+++ src/Source.cpp
@@ -4,34 +4,36 @@
 #include <fstream>
 #include <sstream>
 
+// basic file operations
+#include <iostream>
+#include <fstream>
+
 extern "C"
 {
-#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/timestamp.h>
 }
 
-#define USE_H264BSF 1
-
 int main()
 {
-	AVOutputFormat *ofmt_a = NULL, *ofmt_v = NULL;
+	//no need to allocate if we don't want to add some callbacks or information (e.g. WxH) for raw input
 	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx_v = NULL;
-	AVPacket pkt;
-	int ret, i;
-	int videoindex = -1;
-	int frame_index = 0;
 
 	const char *in_filename = "rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4";
 	const char *out_filename_v = "sample.h264";
 
 	av_register_all();
-	//Input
-	if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+
+	//Open input file and export information from file header (if exists) to AVFormatContext variable
+	int sts = avformat_open_input(&ifmt_ctx, in_filename, 0, 0);
+	if (sts < 0) {
 		printf("Could not open input file.");
 		goto end;
 	}
-	if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+
+	sts = avformat_find_stream_info(ifmt_ctx, 0);
+	//if no header, information will be obtained by these call by decoding several frames
+	if (sts < 0) {
 		printf("Failed to retrieve input stream information");
 		goto end;
 	}
@@ -40,21 +42,19 @@ int main()
 	avformat_alloc_output_context2(&ofmt_ctx_v, NULL, NULL, out_filename_v);
 	if (!ofmt_ctx_v) {
 		printf("Could not create output context\n");
-		ret = AVERROR_UNKNOWN;
+		sts = AVERROR_UNKNOWN;
 		goto end;
 	}
-	ofmt_v = ofmt_ctx_v->oformat;
-
-	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+	
+	int videoindex = -1;
+	for (int i = 0; i < ifmt_ctx->nb_streams; i++) {
 		//Create output AVStream according to input AVStream
-		AVFormatContext *ofmt_ctx;
 		AVStream *in_stream = ifmt_ctx->streams[i];
 		AVStream *out_stream = NULL;
 
 		if (ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
 			videoindex = i;
 			out_stream = avformat_new_stream(ofmt_ctx_v, in_stream->codec->codec);
-			ofmt_ctx = ofmt_ctx_v;
 		}
 		else {
 			continue;
@@ -62,7 +62,7 @@ int main()
 
 		if (!out_stream) {
 			printf("Failed allocating output stream\n");
-			ret = AVERROR_UNKNOWN;
+			sts = AVERROR_UNKNOWN;
 			goto end;
 		}
 		//Copy the settings of AVCodecContext
@@ -72,17 +72,11 @@ int main()
 		}
 		out_stream->codec->codec_tag = 0;
 
-		if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+		if (ofmt_ctx_v->oformat->flags & AVFMT_GLOBALHEADER)
 			out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 	}
-	//Dump Format------------------
-	printf("\n==============Input Video=============\n");
-	av_dump_format(ifmt_ctx, 0, in_filename, 0);
-	printf("\n==============Output Video============\n");
-	av_dump_format(ofmt_ctx_v, 0, out_filename_v, 1);
-	printf("\n======================================\n");
 	//Open output file
-	if (!(ofmt_v->flags & AVFMT_NOFILE)) {
+	if (!(ofmt_ctx_v->oformat->flags & AVFMT_NOFILE)) {
 		if (avio_open(&ofmt_ctx_v->pb, out_filename_v, AVIO_FLAG_WRITE) < 0) {
 			printf("Could not open output file '%s'", out_filename_v);
 			goto end;
@@ -95,14 +89,20 @@ int main()
 		goto end;
 	}
 
-#if USE_H264BSF
-	AVBitStreamFilterContext* h264bsfc = av_bitstream_filter_init("h264_mp4toannexb");
-#endif
-
+	//current frame index
+	int frame_index = 0;
+	/*
+	Encoded package. For video, it should typically contain one compressed frame
+	*/
+	AVPacket pkt;
 	while (1) {
-		AVFormatContext *ofmt_ctx;
 		AVStream *in_stream, *out_stream;
-		//Get an AVPacket
+		/*
+		Get an AVPacket containing encoded data for one AVStream, identified by AVPacket.stream_index (Return the next frame of a audio/video stream)
+		This function returns what is stored in the file, and does not validate that what is there are valid frames for the decoder.
+		It will split what is stored in the file into frames and return one for each call.
+		It will not omit invalid data between valid frames so as to give the decoder the maximum information possible for decoding.
+		*/
 		if (av_read_frame(ifmt_ctx, &pkt) < 0)
 			break;
 		in_stream = ifmt_ctx->streams[pkt.stream_index];
@@ -110,51 +110,48 @@ int main()
 
 		if (pkt.stream_index == videoindex) {
 			out_stream = ofmt_ctx_v->streams[0];
-			ofmt_ctx = ofmt_ctx_v;
-#if USE_H264BSF
-			av_bitstream_filter_filter(h264bsfc, in_stream->codec, NULL, &pkt.data, &pkt.size, pkt.data, pkt.size, 0);
-#endif
 			printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);
 		}
 		else {
 			continue;
 		}
-
-
-		//Convert PTS/DTS
+		/*
+		Convert PTS/DTS
+		The timing information (AVPacket.pts, AVPacket.dts and AVPacket.duration) is in AVStream.time_base units, 
+		i.e. it has to be multiplied by the timebase to convert them to seconds.
+		PTS/DTS are needed to resolve decode/display order issue - PTS timestamp for display, DTS - timestamp for decode, they are equal in case
+		of not B frames
+		*/
 		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
 		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-		pkt.pos = -1;
+		pkt.duration = av_rescale_q_rnd(pkt.duration, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF);
+		//in our output file only 1 stream is available with index 0
 		pkt.stream_index = 0;
 		//Write
-		if (av_interleaved_write_frame(ofmt_ctx, &pkt) < 0) {
+		if (av_write_frame(ofmt_ctx_v, &pkt) < 0) {
 			printf("Error muxing packet\n");
 			break;
 		}
+
 		printf("Write %8d frames to output file\n", frame_index);
 		av_free_packet(&pkt);
 		frame_index++;
+		if (frame_index == 2)
+			break;
 	}
-#if USE_H264BSF
-	av_bitstream_filter_close(h264bsfc);
-#endif
-
-	//Write file trailer
+	//Flush buffered data
 	av_write_trailer(ofmt_ctx_v);
 end:
 	avformat_close_input(&ifmt_ctx);
 	/* close output */
-	if (ofmt_ctx_v && !(ofmt_v->flags & AVFMT_NOFILE))
+	if (ofmt_ctx_v && !(ofmt_ctx_v->oformat->flags & AVFMT_NOFILE))
 		avio_close(ofmt_ctx_v->pb);
 
 	avformat_free_context(ofmt_ctx_v);
 
-	system("pause");
-	if (ret < 0 && ret != AVERROR_EOF) {
+	if (sts < 0 && sts != AVERROR_EOF) {
 		printf("Error occurred.\n");
 		return -1;
 	}
-
 	return 0;
 }
