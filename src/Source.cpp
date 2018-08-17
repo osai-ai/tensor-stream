c60458bd9ba0115b6fd76cd723500b6380282b49
Bykadorov, Roman <prosto_mail95@mail.ru>
2018-08-17 12:19:09 +0300

    [Parser] Cleaned parser code

--- src/Source.cpp
+++ src/Source.cpp
@@ -16,15 +16,14 @@ extern "C"
 int main()
 {
 	AVOutputFormat *ofmt_a = NULL, *ofmt_v = NULL;
-	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx_a = NULL, *ofmt_ctx_v = NULL;
+	AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx_v = NULL;
 	AVPacket pkt;
 	int ret, i;
-	int videoindex = -1, audioindex = -1;
+	int videoindex = -1;
 	int frame_index = 0;
 
 	const char *in_filename = "rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4";
 	const char *out_filename_v = "sample.h264";
-	const char *out_filename_a = "sample.mp3";
 
 	av_register_all();
 	//Input
@@ -46,15 +45,6 @@ int main()
 	}
 	ofmt_v = ofmt_ctx_v->oformat;
 
-
-	avformat_alloc_output_context2(&ofmt_ctx_a, NULL, NULL, out_filename_a);
-	if (!ofmt_ctx_a) {
-		printf("Could not create output context\n");
-		ret = AVERROR_UNKNOWN;
-		goto end;
-	}
-	ofmt_a = ofmt_ctx_a->oformat;
-
 	for (i = 0; i < ifmt_ctx->nb_streams; i++) {
 		//Create output AVStream according to input AVStream
 		AVFormatContext *ofmt_ctx;
@@ -66,13 +56,8 @@ int main()
 			out_stream = avformat_new_stream(ofmt_ctx_v, in_stream->codec->codec);
 			ofmt_ctx = ofmt_ctx_v;
 		}
-		else if (ifmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
-			audioindex = i;
-			out_stream = avformat_new_stream(ofmt_ctx_a, in_stream->codec->codec);
-			ofmt_ctx = ofmt_ctx_a;
-		}
 		else {
-			break;
+			continue;
 		}
 
 		if (!out_stream) {
@@ -95,20 +80,11 @@ int main()
 	av_dump_format(ifmt_ctx, 0, in_filename, 0);
 	printf("\n==============Output Video============\n");
 	av_dump_format(ofmt_ctx_v, 0, out_filename_v, 1);
-	printf("\n==============Output Audio============\n");
-	av_dump_format(ofmt_ctx_a, 0, out_filename_a, 1);
 	printf("\n======================================\n");
 	//Open output file
 	if (!(ofmt_v->flags & AVFMT_NOFILE)) {
 		if (avio_open(&ofmt_ctx_v->pb, out_filename_v, AVIO_FLAG_WRITE) < 0) {
 			printf("Could not open output file '%s'", out_filename_v);
-			goto end;
-		}
-	}
-
-	if (!(ofmt_a->flags & AVFMT_NOFILE)) {
-		if (avio_open(&ofmt_ctx_a->pb, out_filename_a, AVIO_FLAG_WRITE) < 0) {
-			printf("Could not open output file '%s'", out_filename_a);
 			goto end;
 		}
 	}
@@ -119,11 +95,6 @@ int main()
 		goto end;
 	}
 
-	/*if (avformat_write_header(ofmt_ctx_a, NULL) < 0) {
-		printf("Error occurred when opening audio output file\n");
-		goto end;
-	}
-	*/
 #if USE_H264BSF
 	AVBitStreamFilterContext* h264bsfc = av_bitstream_filter_init("h264_mp4toannexb");
 #endif
@@ -145,12 +116,6 @@ int main()
 #endif
 			printf("Write Video Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);
 		}
-		/*else if (pkt.stream_index == audioindex) {
-			out_stream = ofmt_ctx_a->streams[0];
-			ofmt_ctx = ofmt_ctx_a;
-			printf("Write Audio Packet. size:%d\tpts:%lld\n", pkt.size, pkt.pts);
-		}
-		*/
 		else {
 			continue;
 		}
@@ -176,18 +141,13 @@ int main()
 #endif
 
 	//Write file trailer
-	av_write_trailer(ofmt_ctx_a);
 	av_write_trailer(ofmt_ctx_v);
 end:
 	avformat_close_input(&ifmt_ctx);
 	/* close output */
-	if (ofmt_ctx_a && !(ofmt_a->flags & AVFMT_NOFILE))
-		avio_close(ofmt_ctx_a->pb);
-
 	if (ofmt_ctx_v && !(ofmt_v->flags & AVFMT_NOFILE))
 		avio_close(ofmt_ctx_v->pb);
 
-	avformat_free_context(ofmt_ctx_a);
 	avformat_free_context(ofmt_ctx_v);
 
 	system("pause");
