2706df9f43931721be097b8550a567a0583c882b
Bykadorov, Roman <prosto_mail95@mail.ru>
2019-01-15 17:31:16 +0300

    Added few tests and recources folder

--- tests/src/ParserTests.cpp
+++ tests/src/ParserTests.cpp
@@ -5,12 +5,28 @@ int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
 	//	::testing::GTEST_FLAG(filter) = "Pointer.Callstack";
+	
+	//Disable cout output from library
+	std::ofstream   fout("/dev/null");
+	std::cout.rdbuf(fout.rdbuf());
+	
 	return RUN_ALL_TESTS();
 	std::getchar(); // keep console window open until Return keystroke
 }
 
-TEST(Parser, WrongInputArgs) {
+TEST(Parser, WrongInputPath) {
 	Parser parser;
 	ParserParameters parserArgs = { "wrong_path" };
+	EXPECT_NE(parser.Init(parserArgs), VREADER_OK);
+	parserArgs = { };
+	EXPECT_NE(parser.Init(parserArgs), VREADER_OK);
+}
+
+TEST(Parser, CorrectInputPath) {
+	Parser parser;
+	ParserParameters parserArgs = { "rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4" };
+	EXPECT_EQ(parser.Init(parserArgs), VREADER_OK);
+	parser.Close();
+	parserArgs = { "../resources/bbb_1080x608_10.h264" };
 	EXPECT_EQ(parser.Init(parserArgs), VREADER_OK);
 }
