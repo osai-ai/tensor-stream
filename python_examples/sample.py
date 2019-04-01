7db4a7f2bfe8f65866451d6d8b697492056f4ed5
Ruslan Baikulov <ruslan1123@gmail.com>
2019-04-01 16:45:53 +0300

    refactor: Python sample refactoring

--- python_examples/sample.py
+++ python_examples/sample.py
@@ -3,16 +3,32 @@ from tensor_stream import TensorStreamConverter, LogsLevel, LogsType, FourCC
 import argparse
 import os
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('--help', action='help', help='show this help message and exit')
-parser.add_argument("-i", "--input", default="rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4", help="Path to bitstream: RTMP, local file")
-parser.add_argument("-o", "--output", help="Name of output raw stream")
-parser.add_argument("-w", "--width", help="Output width (default: input bitstream width)", type=int)
-parser.add_argument("-h", "--height", help="Output height (default: input bitstream height)", type=int)
-parser.add_argument("-fc", "--fourcc", default="RGB24", choices=["RGB24","BGR24", "Y800"], help="Decoded stream' FourCC (default: RGB24)")
-parser.add_argument("-v", "--verbose", default="LOW", choices=["LOW", "MEDIUM", "HIGH"], help="Set output level from library (default: LOW)")
-parser.add_argument("-n", "--number", help="Number of frame to parse (default: unlimited)", type=int)
-args = parser.parse_args()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--help', action='help',
+                        help='show this help message and exit')
+    parser.add_argument("-i", "--input",
+                        default="rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4",
+                        help="Path to bitstream: RTMP, local file")
+    parser.add_argument("-o", "--output",
+                        help="Name of output raw stream", default="")
+    parser.add_argument("-w", "--width",
+                        help="Output width (default: input bitstream width)",
+                        type=int, default=0)
+    parser.add_argument("-h", "--height",
+                        help="Output height (default: input bitstream height)",
+                        type=int, default=0)
+    parser.add_argument("-fc", "--fourcc", default="RGB24",
+                        choices=["RGB24","BGR24", "Y800"],
+                        help="Decoded stream' FourCC (default: RGB24)")
+    parser.add_argument("-v", "--verbose", default="LOW",
+                        choices=["LOW", "MEDIUM", "HIGH"],
+                        help="Set output level from library (default: LOW)")
+    parser.add_argument("-n", "--number",
+                        help="Number of frame to parse (default: unlimited)", type=int)
+    return parser.parse_args()
+
 
 class DeltaTimeProfiler:
     def __init__(self):
@@ -39,12 +55,11 @@ class DeltaTimeProfiler:
 
 
 if __name__ == '__main__':
+    args = parse_arguments()
+
     reader = TensorStreamConverter(args.input, repeat_number=20)
     reader.enable_logs(LogsLevel[args.verbose], LogsType.CONSOLE)
     reader.initialize()
-
-    result_width = args.width if args.width is not None else reader.frame_size[0]
-    result_height = args.height if args.height is not None else reader.frame_size[1]
 
     reader.start()
     parameters = {
@@ -52,35 +67,30 @@ if __name__ == '__main__':
         'delay': 0,
         'pixel_format': FourCC[args.fourcc],
         'return_index': False,
-        'width': result_width,
-        'height': result_height,
+        'width': args.width,
+        'height': args.height,
     }
+    print("parameters: ", parameters)
 
-    if (args.output is not None):
+    if args.output:
         if os.path.exists(args.output):
             os.remove(args.output)
 
+    profiler = DeltaTimeProfiler()
+    tensor = None
     try:
-        eof = False
-        profiler = DeltaTimeProfiler()
-        while not eof:
+        while True:
             profiler.start()
             tensor = reader.read(**parameters)
             profiler.end()
-            if (args.output is not None):
+            if args.output:
                 reader.dump(tensor, args.output)
-
-            if (args.number is not None):
-                args.number -= 1
-                if (args.number == 0):
-                    break
-
+    except RuntimeError as e:
+        print(f"Bad things happened: {e}")
+    finally:
         print("Frame size: ", reader.frame_size)
         print("FPS: ", reader.fps)
-        print("Mean latancy:", profiler.mean)
-        print("Tensor shape:", tensor.shape)
-
+        print("Mean latency:", profiler.mean)
+        if tensor is not None:
+            print("Tensor shape:", tensor.shape)
         reader.stop()
-    except RuntimeError as e:
-           print("Bad things happened: " + str(e) + "\n")
-           reader.stop()
