70c29962797bcca3205a1e64ff682740f96e0bae
Bykadorov, Roman <prosto_mail95@mail.ru>
2019-07-16 12:09:14 +0300

    TensorStream can be closed without initialization

--- src/Wrappers/WrapperC.cpp
+++ src/Wrappers/WrapperC.cpp
@@ -201,9 +201,12 @@ void TensorStream::endProcessing() {
 		SET_CUDA_DEVICE_THROW();
 		PUSH_RANGE("TensorStream::endProcessing", NVTXColors::GREEN);
 		LOG_VALUE(std::string("End processing sync part start"), LogsLevel::LOW);
-		parser->Close();
-		decoder->Close();
-		vpp->Close();
+		if (parser)
+			parser->Close();
+		if (decoder)
+			decoder->Close();
+		if (vpp)
+			vpp->Close();
 		for (auto& item : processedArr)
 			av_frame_free(&item.second);
 		for (auto& item : decodedArr)
