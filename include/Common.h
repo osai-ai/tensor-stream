47b024780616ae2de547ba3ca0cd64e0976a08bb
Bykadorov, Roman <bykadorov.r@gmail.com>
2019-12-20 15:58:43 +0300

    Added timeout

--- include/Common.h
+++ include/Common.h
@@ -218,6 +218,7 @@ public:
 
 const int defaultCUDADevice = 0;
 const int frameRateConstraints = 240;
+const int defaultTimeout = 10000;
 
 template <class T>
 T findFree(std::string consumerName, std::vector<std::pair<std::string, T> >& entities) {
