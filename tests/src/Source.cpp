b66fc480665e086657e9e7a7cf1580e0f721bb78
BykadorovR <bykadorov.r@gmail.com>
2020-04-06 11:15:20 +0300

    fixed unit tests

--- tests/src/Source.cpp
+++ tests/src/Source.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
-	//::testing::GTEST_FLAG(filter) = "Python_Tests.Crop_NV12_Downscale_Center";
+	//::testing::GTEST_FLAG(filter) = "Parser_Analyze_Broken.LastFrameRepeat";
 
 	//Disable cout output from library
 	std::cout.setstate(std::ios_base::failbit);
