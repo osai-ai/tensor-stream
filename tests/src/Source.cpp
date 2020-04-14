2f7e1a33f73145bf31c82d4497aae3852a10abf1
BykadorovR <bykadorov.r@gmail.com>
2020-04-14 16:28:47 +0300

    Extended Python tests

--- tests/src/Source.cpp
+++ tests/src/Source.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
-	//::testing::GTEST_FLAG(filter) = "Python_Tests.Batch_FourCC_NV12";
+	//::testing::GTEST_FLAG(filter) = "Wrapper_Batch.InstanceCPUMemory";
 
 	//Disable cout output from library
 	std::cout.setstate(std::ios_base::failbit);
