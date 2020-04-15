b01ca48f073e24a69730a9d3edcc51a8569f3e2d
BykadorovR <bykadorov.r@gmail.com>
2020-04-15 12:51:49 +0300

    Fixed RAM memory leak, added tests

--- src/Wrappers/WrapperPython.cpp
+++ src/Wrappers/WrapperPython.cpp
@@ -393,9 +393,11 @@ at::Tensor TensorStream::getFrameAbsolute(std::vector<int> index, FrameParameter
 
 				sts = avcodec_receive_frame(decoder->getDecoderContext(), decoded);
 
+				int currentPTS = readFrames.first->pts;
+				av_packet_unref(readFrames.first);
 				if (sts == AVERROR(EAGAIN) || sts == AVERROR_EOF) {
 					//we found needed frame, need to drain decoder until he returns us desired frame
-					if (pts == readFrames.first->pts) {
+					if (pts == currentPTS) {
 						while (pts != decoded->pts) {
 							sts = avcodec_send_packet(decoder->getDecoderContext(), nullptr);
 							sts = avcodec_receive_frame(decoder->getDecoderContext(), decoded);
--- tests/src/Source.cpp
+++ tests/src/Source.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
-	::testing::GTEST_FLAG(filter) = "Wrapper_Batch.InstanceCPUMemory";
+	//::testing::GTEST_FLAG(filter) = "Wrapper_Batch.ReadCPUMemory";
 
 	//Disable cout output from library
 	std::cout.setstate(std::ios_base::failbit);
