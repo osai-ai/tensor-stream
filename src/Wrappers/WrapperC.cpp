bea529bce323c15d2c41c8389579ba4366567df9
BykadorovR <bykadorov.r@gmail.com>
2020-06-02 14:10:07 +0300

    Removed flush

--- src/Wrappers/WrapperC.cpp
+++ src/Wrappers/WrapperC.cpp
@@ -370,7 +370,8 @@ std::vector<T*> TensorStream::getFrameAbsolute(std::vector<int> index, FramePara
 	bool flushed = false;
 	uint64_t currentPTS;
 	auto videoStream = parser->getFormatContext()->streams[parser->getVideoIndex()];
-	const int distanceForSeek = 20;
+	//TODO: in case of SW decoder if required frame is very close to I frame, we should set not big number of threads otherwise we will stuck at feeding decoder with
+	//frames because it uses frame parallel decoding not slice
 	for (int i = 0; i < index.size(); i++) {
 		START_LOG_BLOCK(std::string("GetFrameAbsolute iteration"));
 		{
@@ -388,11 +389,14 @@ std::vector<T*> TensorStream::getFrameAbsolute(std::vector<int> index, FramePara
 			int multiplier = index[i] / gopSize;
 			int intraIndex = multiplier * gopSize;
 			//if first frame or flushed or needed frame is behind current decoded or 
-			if (i == 0 || flushed || pts < outputDTS.back() || intraIndex - PTSToFrame(videoStream, outputDTS.back()) > distanceForSeek) {
+			if (i == 0 || flushed || pts < outputDTS.back() || intraIndex > PTSToFrame(videoStream, outputDTS.back())) {
+				/*
 				if (flushed)
 					flushed = false;
 				else
+					//TODO: maybe it will be faster to decode rest of "unrelated" frames after seek than reset HW decoder
 					avcodec_flush_buffers(decoder->getDecoderContext());
+				*/
 				//seek to desired frame
 				sts = av_seek_frame(parser->getFormatContext(), parser->getVideoIndex(), pts, AVSEEK_FLAG_BACKWARD);
 			}
